08679f96e78df84351d8167e24e72685a77f442a
Achu <achu.zekamashi@gmail.com>
2017-11-20 00:14:06 +0900

    Unityちゃんのことばを追加

---
+++ Assets/MessageManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine.UI;
+
+public class MessageManager : MonoBehaviour {
+
+	private Text m_text;
+	private 
+
+	void Start () {
+		this.UpdateAsObservable ()
+			.Where (_ => this.enabled == true)
+			.Where (_ => Time.time > 5.0f)
+			.First ()
+			.Subscribe (_ => StartCoroutine (NextMessage()));
+
+		m_text = GetComponent<Text> ();
+	}
+
+	IEnumerator NextMessage() {
+		yield return new WaitForSeconds (3.0f);
+		m_text.text = "Unityを使って\n" +
+			"I wanted to ";
+		
+		yield return new WaitForSeconds (3.0f);
+		m_text.text = "プレゼンをする\nつもりだったけど\n" +
+		"introduce myself\n with Unity";
+
+		yield return new WaitForSeconds (4.0f);
+		m_text.text = "時間がなかったので\n作れませんでした\n" +
+		"but I had no\n time to make it";
+
+		yield return new WaitForSeconds (4.0f);
+		m_text.text = "申し訳ございません\n" +
+		"I'm sorry.";
+	}	
+
+}
--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -8,17 +8,19 @@ public class SceneManager : MonoBehaviour {
 
 	[SerializeField]private GameObject m_player;
 	[SerializeField]private Image m_image;
+	[SerializeField]private GameObject m_message;
 	[SerializeField]private int m_count = 0;
 	void Start () {
 
 		float m_alpha = 1.0f;
 		Observable.Timer (System.TimeSpan.FromSeconds (1.0f))
 			.Subscribe (x => {
+				m_message.SetActive (false);
 				this.UpdateAsObservable()
 					.Where(_ => m_image.color.a > 0)
 					.Where(_ => m_count == 0)
 					.Subscribe(_ => {
-						m_image.color = new Color(0,0,0, m_alpha);
+						m_image.color = new Color(0, 0, 0, m_alpha);
 						m_alpha -= Time.deltaTime;
 					});
 		});
@@ -29,7 +31,11 @@ public class SceneManager : MonoBehaviour {
 			.First ()
 			.Subscribe (_ => {
 				m_player.GetComponent<Animator>().enabled = true;
-				m_count++;		
+				m_count++;
+				Observable.Timer(System.TimeSpan.FromSeconds(5.5f))
+					.Subscribe(x => {
+						m_message.SetActive(true);
+					});
 		});
 
 	}
