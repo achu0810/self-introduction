ae1e17cbcdbfaf6c88acf6a04dd27aa8595b47eb
Achu <achu.zekamashi@gmail.com>
2017-11-20 19:04:50 +0900

    VideoPanelへのカメラアニメーションを追加

--- Assets/Scripts/MessageManager.cs
+++ Assets/Scripts/MessageManager.cs
@@ -7,9 +7,12 @@ using UnityEngine.UI;
 public class MessageManager : MonoBehaviour {
 
 	private Text m_text;
-	private 
+	private GameObject m_message;
+	[SerializeField]private Image m_image;
 
 	void Start () {
+		m_message = transform.root.gameObject;
+
 		this.UpdateAsObservable ()
 			.Where (_ => this.enabled == true)
 			.Where (_ => Time.time > 5.0f)
@@ -39,6 +42,20 @@ public class MessageManager : MonoBehaviour {
 		yield return new WaitForSeconds (3.0f);
 		m_text.text = "以下普通の自己紹介\n" +
 		"I introduce myself\n the same as usual";
+
+		yield return new WaitForSeconds (3.5f);
+		StartCoroutine (FadeoutImage());
 	}	
+
+	IEnumerator FadeoutImage() {
+		float m_alpha = 1.0f;
+		while (m_image.color.a > 0) {
+			m_text.color = m_image.color = new Color (0, 0, 0, m_alpha);
+			m_alpha -= Time.deltaTime;
+			yield return null;
+		}
+		Animator m_animator = GameObject.FindWithTag ("MainCamera").GetComponent<Animator> ();
+		m_animator.SetTrigger ("ToVideoPanel");
+	}
 
 }
